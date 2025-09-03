7bf20765aebbda8f7d19a18296d208720726d8c0
xandy <alexandre.inascimento@aluno.edu.es.gov.br>
2025-09-03 11:06:01 -0300

    Create Aula2.php

---
+++ Aula 2-3trimestre/Aula2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Aula 02 php</title>
+    </head>
+    <body>
+        <?php
+      $nome1="gustavo";
+      $nome2="alexandre";
+
+      echo "Meu nome é" .$nome1 ."e" minha dupla é" .$nome2;
+         
+        ?>
+    </body>
+</html>
