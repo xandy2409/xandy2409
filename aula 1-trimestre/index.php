10d21e7266df28c3c838bedfa88ef402d1096438
xandy <alexandre.inascimento@aluno.edu.es.gov.br>
2025-09-03 10:52:18 -0300

    Create index.php

---
+++ aula 1-trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 php</title>
+    </head>
+    <body>
+     <?php
+         echo "olá, mundo!"; 
+         echo "alexandre e gustavo";
+     ?>
+    </body>
+</html>
